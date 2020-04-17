6ff35127b47c5390ecb9600a5c92a52ee1892e3b
Ian Glennon <iglennon@qualys.com>
2020-04-17 15:16:51 +0100

    Added QualysOptionProfileProcessor.py and testOptionProfiles.py Updated test.py with Option Profiles test Updated args list in test.py to remove all single-letter args from tests to ensure explicit testing behaviour

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import testIPs
 import testDomains
 import testNetworks
 import testAssetGroups
+import testOptionProfiles
 
 
 def quit(exitcode: int):
@@ -32,13 +33,13 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
     parser.add_argument('-s', '--simulate', action='store_true', help='Simulate - obtain data from source, do not send '
                                                                       'to target')
-    parser.add_argument('-T', '--testTags', action='store_true', help='Test Tag Migration')
-    parser.add_argument('-S', '--testSubscription', action='store_true', help='Test Subscription Configuration '
-                                                                              'Migration')
-    parser.add_argument('-I', '--testIPs', action='store_true', help='Test IP Migration')
-    parser.add_argument('-D', '--testDomains', action='store_true', help='Test Domains Migration')
-    parser.add_argument('-N', '--testNetworks', action='store_true', help='Test Networks Migration')
-    parser.add_argument('-A', '--testAssetGroups', action='store_true', help='Test Asset Groups Migration')
+    parser.add_argument('--testTags', action='store_true', help='Test Tag Migration')
+    parser.add_argument('--testSubscription', action='store_true', help='Test Subscription Configuration Migration')
+    parser.add_argument('--testIPs', action='store_true', help='Test IP Migration')
+    parser.add_argument('--testDomains', action='store_true', help='Test Domains Migration')
+    parser.add_argument('--testNetworks', action='store_true', help='Test Networks Migration')
+    parser.add_argument('--testAssetGroups', action='store_true', help='Test Asset Groups Migration')
+    parser.add_argument('--testOptionProfiles', action='store_true', help='Test Option Profiles Migration')
 
     args = parser.parse_args()
 
@@ -121,3 +122,9 @@ if __name__ == '__main__':
             print('Asset Groups test failed')
             quit(1)
 
+    # Option Profiles
+    if args.testOptionProfiles:
+        if not testOptionProfiles.testOptionProfiles(source_api=source_api, target_api=target_api,
+                                                     simulate=args.simulate):
+            print('Option Profiles test failes')
+            quit(1)
