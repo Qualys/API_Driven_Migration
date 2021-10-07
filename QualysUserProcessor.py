2ebe320c6c66cef9bd3dc56484f6c76655af2fef
glennog <ianglennon@gmail.com>
2021-10-07 15:37:36 +0100

    QualysUserProcessor.py  - Fixed user processing:      * now correctly identifies contact information (bugfix)      * reset the URL variable for each user (bugfix)      * will no longer attempt to assign 'Unassigned' Business Unit to user      * now correctly identifies asset group assignments where applied
     - Added simulation and send_email options to createUsers()

--- QualysUserProcessor.py
+++ QualysUserProcessor.py
@@ -8,34 +8,48 @@ def responseHandler(resp):
 
 def generateURLs(userlist: ET.Element):
     fullurl = ''
+
     user_attribs = {
         'user_role': 'USER_ROLE',
         'business_unit': 'BUSINESS_UNIT',
-        'asset_groups': '',
-        'first_name': 'FIRSTNAME',
-        'last_name': 'LASTNAME',
-        'title': 'TITLE',
-        'phone': 'PHONE',
-        'fax': 'FAX',
-        'email': 'EMAIL',
-        'company': 'COMPANY',
-        'address1': 'ADDRESS1',
-        'address2': 'ADDRESS2',
-        'city': 'CITY',
-        'country': 'COUNTRY',
-        'state': 'STATE',
-        'zip_code': 'ZIP_CODE',
+        'first_name': 'CONTACT_INFO/FIRSTNAME',
+        'last_name': 'CONTACT_INFO/LASTNAME',
+        'title': 'CONTACT_INFO/TITLE',
+        'phone': 'CONTACT_INFO/PHONE',
+        'fax': 'CONTACT_INFO/FAX',
+        'email': 'CONTACT_INFO/EMAIL',
+        'company': 'CONTACT_INFO/COMPANY',
+        'address1': 'CONTACT_INFO/ADDRESS1',
+        'address2': 'CONTACT_INFO/ADDRESS2',
+        'city': 'CONTACT_INFO/CITY',
+        'country': 'CONTACT_INFO/COUNTRY',
+        'state': 'CONTACT_INFO/STATE',
+        'zip_code': 'CONTACT_INFO/ZIP_CODE',
+        'time_zone_code': 'CONTACT_INFO/TIME_ZONE_CODE',
         'external_id': 'EXTERNAL_ID'
     }
 
     urllist = []
 
     for user in userlist.findall('USER'):
+        fullurl = ''
         for attrib in user_attribs.keys():
             if user.find(user_attribs[attrib]) is not None:
-                fullurl = '%s&%s=%s' % (fullurl, attrib, user.find(user_attribs[attrib]).text)
-                fullurl.replace(' ','+')
-                urllist.append(fullurl)
+                attrib_value = user.find(user_attribs[attrib]).text
+                if attrib == 'business_unit':
+                    if attrib_value != 'Unassigned':
+                        fullurl = '%s&%s=%s' % (fullurl, attrib, attrib_value)
+                else:
+                    fullurl = '%s&%s=%s' % (fullurl, attrib, user.find(user_attribs[attrib]).text)
+
+        if user.find('ASSIGNED_ASSET_GROUPS') is not None:
+            asset_group_list = []
+            for asset_group in user.findall('.//ASSET_GROUP_TITLE'):
+                asset_group_list.append(asset_group.text)
+            fullurl = '%s&asset_groups=%s' % (fullurl, ','.join(asset_group_list))
+
+        fullurl = fullurl.replace(' ', '+')
+        urllist.append(fullurl)
 
     return urllist
 
@@ -49,11 +63,20 @@ def getUsers(source_api: QualysAPI.QualysAPI):
     return resp.find('USER_LIST')
 
 
-def createUsers(target_api: QualysAPI.QualysAPI, userlist: list):
-    for url in userlist:
-        fullurl = '%s/msp/user.php?action=add&send_email=1%s' % (target_api.server, url)
-        resp = target_api.makeCall(url=fullurl)
-        if not responseHandler(resp):
-            print('QualysUserProcessor.createUsers failed')
-            return False
+def createUsers(target_api: QualysAPI.QualysAPI, urllist: list, send_email: bool = True, simulate: bool = False):
+    if send_email:
+        email = 'send_email=1'
+    else:
+        email = 'send_email=0'
+    if simulate:
+        print("QualysUserProcessor Simulation Output")
+    for url in urllist:
+        fullurl = '%s/msp/user.php?action=add&%s%s' % (target_api.server, email, url)
+        if simulate:
+            print(fullurl)
+        else:
+            resp = target_api.makeCall(url=fullurl)
+            if not responseHandler(resp):
+                print('QualysUserProcessor.createUsers failed')
+                return False
     return True
